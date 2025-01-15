380f1e8108eb2247849d5886fe384c87a5c82cee
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2025-01-15 14:41:08 +0900

    ステージ完成（ひとまず）

--- Client/Assets/Scripts/Character.cs
+++ Client/Assets/Scripts/Character.cs
@@ -14,8 +14,7 @@ public class Character : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        //floatingJoystick = GameObject.Find("Player(Clone)").GetComponent<FloatingJoystick>();
-        floatingJoystick = GameObject.Find("Player").GetComponent<FloatingJoystick>();
+        floatingJoystick = GameObject.Find("Player(Clone)").GetComponent<FloatingJoystick>();
     }
 
     // Update is called once per frame
